f2b6c52dfbea30134cf7fee9cf848188ae0a4faf
Star Yznaga <wyznaga@gmail.com>
2020-01-15 20:00:08 -0600

    feat: wip image grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fractaly';
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 1},
+    {text: 'Two', cols: 1, rows: 1},
+    {text: 'Three', cols: 1, rows: 1},
+    {text: 'Four', cols: 1, rows: 1},
+    {text: 'Five', cols: 1, rows: 1},
+    {text: 'Six', cols: 1, rows: 1},
+    {text: 'Seven', cols: 1, rows: 1},
+    {text: 'Eight', cols: 1, rows: 1},
+    {text: 'Nine', cols: 1, rows: 1},
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule, MatCardModule, MatGridListModule, MatToolbarModule } from '@angular/material';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
